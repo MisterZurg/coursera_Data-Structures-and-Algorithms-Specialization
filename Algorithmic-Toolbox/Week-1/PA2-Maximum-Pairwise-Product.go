61c99c580d6c1666fa4015487a6547c1f6c975de
MisterZurg <misterzurg@gmail.com>
2021-09-05 20:54:05 +0300

    Initial commit

---
+++ Algorithmic-Toolbox/Week-1/PA2-Maximum-Pairwise-Product.go
@@ -0,0 +1,57 @@
+/*
+In this programming challenge,
+your goal is to implement a program that works in less than one second even on huge datasets.
+*/
+package Week_1
+
+import (
+	"fmt"
+	"math/big"
+)
+
+func MaximumPairwiseProduct(){
+	var n int
+	fmt.Scan(&n)
+
+	var inputDataSet = make([]int64, n, n)
+
+	// Заполним слайс значениями
+	for i := 0; i < n; i++ {
+		fmt.Scan(&inputDataSet[i])
+	}
+
+	var imax1, imax2, imin1, imin2 int = -1, -1, -1, -1
+	var firstCheck = true
+	for idx, num := range inputDataSet {
+		if firstCheck {
+			firstCheck = false
+			imax1 = idx
+			imin1 = idx
+			continue
+		}
+		if num > inputDataSet[imax1] {
+			imax2 = imax1
+			imax1 = idx
+			// Первым условием мы обходим несуществующий индекс
+		} else if imax2 == -1 || num >= inputDataSet[imax2] {
+			imax2 = idx
+		}
+		// Min
+		if num < inputDataSet[imin1] {
+			imin2 = imin1
+			imin1 = idx
+			// Первым условием мы обходим несуществующий индекс
+		} else if imin2 == -1 || num <= inputDataSet[imin2] {
+			imin2 = idx
+		}
+	}
+	//fmt.Println(imax1, imax2, imin1, imin2)
+	if inputDataSet[imin2]*inputDataSet[imin1] > inputDataSet[imax2]*inputDataSet[imax1] {
+		// Произведение минимальных
+		fmt.Println(big.NewInt(int64(inputDataSet[imax2]) * int64(inputDataSet[imax1])))
+	} else {
+		// Произведение максимальных
+		fmt.Println(big.NewInt(int64(inputDataSet[imax2]) * int64(inputDataSet[imax1])))
+	}
+}
+
